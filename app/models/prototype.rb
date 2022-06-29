98e79da4623b0ef757fce3d3b8966178c698c813
naokiebihara <102887633+naokiebihara@users.noreply.github.com>
2022-06-29 20:47:32 +0900

    訂正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -7,4 +7,11 @@ class Prototype < ApplicationRecord
   validates :catch_copy, presence: true
   validates :concept, presence: true
   validates :image, presence: true
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
